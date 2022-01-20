7190a859970d8d4939fc109f54bf7a9cab8548d1
knecht <ldj910505@naver.com>
2022-01-20 15:38:11 +0900

    code : 2일차 학습 자료 업로드
    - 가운데손가락 모자이크 알고리즘 만들었다
    
    - 만든 건 아니고 내가 베낀 거다

---
+++ Chapter02/single.py
@@ -0,0 +1,92 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+# 가위바위보 기계 만들기-손가락 인식 인공지능
+# 1. 손가락 인식 MediaPipe
+# 2. 손가락 마디 각도 계산
+# 3. 제스쳐 인식 KNN
+# 4. 승자 결정
+
+# 이 중에서 0(바위), 5(보), 9(가위) 로 이해해서 문제를 풀어 본다.
+
+max_num_hands = 1
+gesture = {
+    0:'fist', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+    6:'six', 7:'rock', 8:'spiderman', 9:'yeah', 10:'ok',
+}
+# 솔직히 사람이 찌를 두 가지 방법으로 하지 않나?
+rps_gesture = {0:'rock', 5:'paper', 2:'scissors', 9:'scissors'}
+
+# MediaPipe hands model
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+# 손가락 볼륨 조절기
+hands = mp_hands.Hands(
+    max_num_hands=max_num_hands,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+# 제스처 인식 모델
+# Gesture recognition model
+file = np.genfromtxt('Chapter02\data\gesture_train.csv', delimiter=',')
+angle = file[:,:-1].astype(np.float32)
+label = file[:, -1].astype(np.float32)
+knn = cv2.ml.KNearest_create()
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)
+
+# 웹캠에서 이미지를 가져온다
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        continue
+
+    img = cv2.flip(img, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    result = hands.process(img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    # 만약에 손을 인식했다.
+    if result.multi_hand_landmarks is not None:
+        for res in result.multi_hand_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
+
+            # Compute angles between joints
+            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
+            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+            # 이 벡터를 빼 주면 관절
+            v = v2 - v1 # [20,3]
+            # Normalize v
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            # Get angle using arcos of dot product
+            # 이 수식을 아는 사람은 말좀해주세요
+            angle = np.arccos(np.einsum('nt,nt->n',
+                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+
+            angle = np.degrees(angle) # Convert radian to degree
+
+            # Inference gesture
+            data = np.array([angle], dtype=np.float32)
+            ret, results, neighbours, dist = knn.findNearest(data, 3)
+            idx = int(results[0][0])
+
+            # Draw gesture result
+            if idx in rps_gesture.keys():
+                cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+            # Other gestures
+            # cv2.putText(img, text=gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('Game', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
